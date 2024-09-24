af763e3e352ea6f3915bdefc462b6b8312593e7b
Jordan Silva <Slvjordan2626@gmail.com>
2024-09-24 14:50:16 -0700

    Added NavBar.vue

---
+++ vue-project/src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar-container">
+            <router-link to="/" class="navbar-logo" @click="closeMobileMenu">
+                PHEONIX CAPITAL GROUP <i class="navbar-logo" />
+            </router-link>
+
+            <div class="menu-icon" @click="handleClick">
+                <i :class="click ? 'fas fa-times' : 'fas fa-bars'" />
+            </div>
+
+        </div>
+    </nav>
+</template>
