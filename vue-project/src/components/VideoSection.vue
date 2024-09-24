f1bfa227650332a6679ae99dfbcd56daba082e2d
Jordan Silva <Slvjordan2626@gmail.com>
2024-09-24 16:49:07 -0700

    Home Screen

---
+++ vue-project/src/components/VideoSection.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="relative w-full h-screen flex flex-col justify-center items-center">
+        <video src="../../public/videos/mountain.mp4" autoplay loop muted
+            class="absolute top-0 left-0 w-full h-full object-cover z-0" />
+        <h1 class="text-white text-[100px] -mt-24 sm:text-[70px] md:text=[50px]">
+            JOIN OUR TEAM
+        </h1>
+        <p class="mt-2 text-white text-2x1 font-sans sm:text-x1">
+            "Together, we'll find solutions"
+        </p>
+        <div class="video-btns mt-8">
+            <button
+                class="bg-transparent border border-white text-white py-2 px-4 rounded hover:bg-white hover:text-black transition duration-300">
+                GET STARTED
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "VideoSection",
+};
+</script>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,8 +1,18 @@
-<script setup>
+<script>
+
+import VideoSection from '../components/VideoSection.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    VideoSection,
+  },
+};
 </script>
 
 <template>
   <main>
-    <p>Home</p>
+    <h1> Welcome to the Home Page! </h1>
+    <VideoSection></VideoSection>
   </main>
 </template>
