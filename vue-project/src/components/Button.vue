6a97fb4c3980b4b5f3fbdcaf11ccc81e066099c4
Jordan Silva <Slvjordan2626@gmail.com>
2024-09-25 12:53:27 -0700

    Added Button

---
+++ vue-project/src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <router-link to="/sign-in" class="inline-block">
+        <button :class="[buttonStyle === 'btn--primary' ? 'bg-white text-gray-900 border border-white hover:bg-gray-100 hover:text-black'
+            : 'bg-transparent text-white border border-white hover:bg-white hover:text-black', buttonSize === 'btn--large' ? 'btn--large' ? 'text-xl py-3 px-6' : 'text-lg py-2 px-4',
+            'font-semibold rounded transition-all duration-300']" @click="onClick" :type="type">
+            <slot></slot>
+        </button>
+    </router-link>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            default, 'button',
+        },
+        onClick: {
+            type: Function,
+            default: null,
+        },
+        buttonStyle: {
+            type: String,
+            default: 'btn--medium',
+        },
+    },
+};
+</script>
--- vue-project/src/components/VideoSection.vue
+++ vue-project/src/components/VideoSection.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="relative w-full h-screen flex flex-col justify-center items-center">
-        <div class="relative Z-10 text-center">
-            <h1 class="text-white text-[100px] -mt-24 sm:text-[70px] md:text=[50px]">
-                JOIN OUR TEAM
-            </h1>
-            <p class="mt-2 text-white text-2x1 font-sans sm:text-x1">
-                "Together, we'll find solutions"
-            </p>
-            <div class="video-btns mt-8">
-                <button
-                    class="bg-transparent border border-white text-white py-2 px-4 rounded hover:bg-white hover:text-black transition duration-300">
-                    GET STARTED
-                </button>
-            </div>
-        </div>
+    <div class="relative w-screen h-screen flex flex-col justify-center items-center text-center">
         <video src="../../public/videos/mountain.mp4" autoplay loop muted
-            class="absolute top-0 left-0 w-full h-full object-cover z-0"></video>
+            class="absolute inset-0 w-full h-full object-cover z-[-1]"></video>
+        <div class="absolute inset-0 bg-black bg-opacity-20 z-[-1]"></div>
+        <h1 class="text-white text-7xl md:text-5xl sm:text-3xl -mt-20 font-bold">
+            JOIN OUR TEAM
+        </h1>
+        <p class="mt-2 text-white text-2xl font-sans sm:text-lg">
+            "Together, we'll find solutions"
+        </p>
+        <div class="mt-8">
+            <button
+                class="bg-transparent border border-white text-white py-3 px-6 rounded hover:bg-white hover:text-black transition duration-300">
+                GET STARTED
+            </button>
+        </div>
     </div>
 </template>
 
 <script>
+
 export default {
-    name: "VideoSection",
+    name: "VideoSection"
 };
 </script>
