01cbd7a73472bb09e8bfaf877ceaeffaacb69f85
Jordan Silva <Slvjordan2626@gmail.com>
2024-09-25 15:28:22 -0700

    Updated OwnersList and OwnersPage

--- vue-project/src/views/OwnersPage.vue
+++ vue-project/src/views/OwnersPage.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-        <CreateOwner />
+        <CreateOwner @data-fetched="fetchData" />
+        <OwnersList :owners="owners" :fetchData="fetchData" />
     </div>
 </template>
 
 <script>
 
+import { ref } from 'vue';
 import CreateOwner from '../components/CreateOwners.vue';
+import OwnersList from '../components/OwnersList.vue';
+import app from '../realmApp';
+
+const owners = ref([]);
+
+const fetchData = async () => {
+    const mongo = app.currentUser.mongoClient("mongodb-atlas");
+    const collection = mongo.db("Owners_DB").collection("Owners");
+    owners.value = await collection.find();
+};
+
+fetchData();
 
 </script>
