dd15082b4b2117922193373ca90257941d96b2ad
Jordan Silva <Slvjordan2626@gmail.com>
2024-09-26 10:51:23 -0700

    Updated LandHoldingList

--- vue-project/src/components/LandHoldingList.vue
+++ vue-project/src/components/LandHoldingList.vue
@@ -1,5 +1,38 @@
 <template>
-
+    <div>
+        <h2 class="text 2xl font-bold mb-4">Land Holdings</h2>
+        <table class="min-w-full table-auto border-collapse">
+            <thead class="bg-grat-200">
+                <tr>
+                    <th class="border px-4 py-2 text-left">Owner Name</th>
+                    <th class="border px-4 py-2 text-left">Legal Entity</th>
+                    <th class="border px-4 py-2 text-left">Net Mineral Acres</th>
+                    <th class="border px-4 py-2 text-left">Mineral Owner Royalty (%)</th>
+                    <th class="border px-4 py-2 text-left">Section</th>
+                    <th class="border px-4 py-2 text-left">Township</th>
+                    <th class="border px-4 py-2 text-left">Range</th>
+                    <th class="border px-4 py-2 text-left">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="holding in landHoldings" :key="holding._id" class="hover:bg-gray-100">
+                    <td class="border px-4 py-2">{{ owners[holding.ownerId] }}</td>
+                    <td class="border px-4 py-2">{{ holding.legalEntity }}</td>
+                    <td class="border px-4 py-2">{{ holding.netMineralAcres }}</td>
+                    <td class="border px-4 py-2">{{ holding.mineralOwnerRoyalty }}</td>
+                    <td class="border px-4 py-2">{{ holding.section }}</td>
+                    <td class="border px-4 py-2">{{ holding.township }}</td>
+                    <td class="border px-4 py-2">{{ holding.range }}</td>
+                    <td class="border px-4 py-2">
+                        <button @click="handleDelete(holding._id)"
+                            class="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700">
+                            Delete
+                        </button>
+                    </td>
+                </tr>>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
