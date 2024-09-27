67b176de1cabb8f08fc5538077a2bc85bf478fff
Jordan Silva <60489493+Jtsilva26@users.noreply.github.com>
2024-09-27 10:15:32 -0700

    Update App.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,43 +1,62 @@
 <template>
-<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <!-- Logo -->
-      <RouterLink to="/" class="text-white text-2xl font-bold" @click="closeMobileMenu">
-        PHOENIX CAPITAL GROUP
+  <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <RouterLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Phoenix Capital Group</span>
       </RouterLink>
 
-      <!-- Mobile Menu Icon -->
-      <div class="md:hidden" @click="handleClick">
-        <i :class="click ? 'fas fa-times text-white' : 'fas fa-bars text-white'"></i>
-      </div>
-
-      <!-- Menu Items -->
-      <ul
-        :class="click ? 'flex flex-col items-center absolute top-20 left-0 w-full bg-gray-900 md:flex md:flex-row md:static md:w-auto' : 'hidden md:flex md:flex-row'">
-        <li class="md:mx-4 my-2 md:my-0">
-          <RouterLink to="/" class="text-white" @click="closeMobileMenu">Home</RouterLink>
-        </li>
-        <li class="md:mx-4 my-2 md:my-0">
-          <RouterLink v-if="user" to="/services" class="text-white" @click="closeMobileMenu">Services</RouterLink>
-          <span v-else class="text-gray-500 cursor-not-allowed">Services (Sign in to access)</span>
-        </li>
-      </ul>
-
-      <!-- Sign In/Out Button -->
-      <div v-if="buttonVisible" class="hidden md:block">
-        <button v-if="user" @click="handleSignOut" class="border border-white text-white py-2 px-6 hover:bg-white hover:text-gray-900 transition duration-300">
+      <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <button v-if="user" @click="handleSignOut" type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           SIGN OUT
         </button>
         <RouterLink v-else to="/signin">
-          <button class="border border-white text-white py-2 px-6 hover:bg-white hover:text-gray-900 transition duration-300">
+          <button type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             SIGN IN
           </button>
         </RouterLink>
+
+        <button data-collapse-toggle="navbar-sticky" type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-sticky" aria-expanded="false" @click="handleClick">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15" />
+          </svg>
+        </button>
+      </div>
+
+      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" :class="click ? 'flex' : 'hidden'" id="navbar-sticky">
+        <ul
+          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <li>
+            <RouterLink to="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">
+              Home
+            </RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/about" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">
+              About
+            </RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/services" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">
+              Services
+            </RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/contact" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">
+              Contact
+            </RouterLink>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
 
-  <!-- Router View -->
   <RouterView />
 </template>
 
