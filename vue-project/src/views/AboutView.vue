9a05b71e38d44f0a7b4482262b36019876defaab
Jordan Silva <Slvjordan2626@gmail.com>
2024-10-04 10:30:04 -0700

    Modified Link to Sign In for About page

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,3 +1,42 @@
+<!-- <template>
+
+          <p class="mt-6 text-xl leading-8 text-gray-600">Our company is built on principles of transparency, integrity, and collaboration. We work closely with our
+            owners to ensure that every investment aligns with their goals and aspirations. Together, we can unlock
+            the potential of land holdings and create opportunities for all stakeholders involved.</p>
+          <p class="mt-6 text-xl leading-8 text-gray-600">Whether you're an experienced investor or new to the world of land holdings, our team is here to guide you
+            every step of the way. Join us today, and let's embark on this exciting journey together!</p>
+          <div class="mt-10 flex">
+            <RouterLink to="/signin"
+              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Join our team
+            </RouterLink>
+          </div>
+        </div>
+        <div class="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
+          <div class="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+            <img src="https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80" alt="" class="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover" />
+          </div>
+          <div class="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
+            <div class="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+              <img src="https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80" alt="" class="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover" />
+            </div>
+            <div class="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
+              <img src="https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&h=842&q=80" alt="" class="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover" />
+            </div>
+            <div class="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
+              <img src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80" alt="" class="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script> -->
+
 <template>
   <div class="bg-gray-900">
 
@@ -98,7 +137,8 @@
                 </li>
               </ul>
               <div class="mt-10 flex">
-                <a href="#" class="text-sm font-semibold leading-6 text-indigo-400">See our job postings <span aria-hidden="true">&rarr;</span></a>
+                <RouterLink to="/signin" class="text-sm font-semibold leading-6 text-indigo-400">Join our journey <span aria-hidden="true">&rarr;</span>
+                </RouterLink>
               </div>
             </div>
           </div>
@@ -171,9 +211,9 @@
 </template>
 
 <script setup>
-import { defineComponent, h, ref } from 'vue'
-import { Dialog, DialogPanel } from '@headlessui/vue'
-import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { defineComponent, h, ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
 import {
   AcademicCapIcon,
   CheckCircleIcon,
