edb85e76c24ab5bc941f52f1aeb3d26324c590d0
Jordan Silva <Slvjordan2626@gmail.com>
2024-10-07 16:27:33 -0700

    Added Landholding name and section name to table

--- vue-project/src/components/LandHoldingList.vue
+++ vue-project/src/components/LandHoldingList.vue
@@ -9,9 +9,11 @@
               <thead>
                 <tr>
                   <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">Owner Name</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Land Holding Name</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Legal Entity</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Net Mineral Acres</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Mineral Owner Royalty (%)</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Section Name</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Section</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Township</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Range</th>
@@ -24,9 +26,11 @@
               <tbody class="bg-white">
                 <tr v-for="holding in landHoldingsStore.landHoldings" :key="holding._id" class="even:bg-gray-50">
                   <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{{ landHoldingsStore.owners[holding.ownerId] }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ holding.name }}</td>
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ holding.legalEntity }}</td>
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ holding.netMineralAcres }}</td>
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ holding.mineralOwnerRoyalty }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ holding.sectionName }}</td>
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ holding.section }}</td>
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ holding.township }}</td>
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ holding.range }}</td>
